e93a1c2c636b712866d8adcf5e6d2726191cc57c
Malik Enes Şafak <e.maliksafak@gmail.com>
2022-05-29 17:22:24 +0300

    Initial commit

---
+++ include/RingBuffer.h
@@ -0,0 +1,85 @@
+#ifndef RINGBUFFER_H
+#define RINGBUFFER_H
+
+template<class T>
+class RingBuffer
+{
+private:
+    T* buffer = nullptr;
+    size_t length = 0;
+    size_t read_index = 0;
+    size_t write_index = 0;
+    size_t readable_length = 0;
+public:
+    RingBuffer(size_t _length);
+    ~RingBuffer();
+    int write(T* _values, size_t _length);
+    int read(T* _buffer, size_t _length);
+    size_t readable();
+    void reset();
+    void flush();
+};
+
+template<class T>
+inline RingBuffer<T>::RingBuffer(size_t _length)
+{
+    buffer = new T[_length];
+    length = _length;
+    read_index = 0;
+    write_index = 0;
+    readable_length = 0;
+}
+
+template<class T>
+inline RingBuffer<T>::~RingBuffer()
+{
+    delete[] buffer;
+}
+
+template<class T>
+inline int RingBuffer<T>::write(T* _values, size_t _length) {
+    if (readable_length + _length > length) {
+        return 0;
+    }
+    for (size_t i = 0; i < _length; i++) {
+        buffer[(i + write_index) % length] = _values[i];
+    }
+    readable_length += _length;
+    write_index = (write_index + _length) % length;
+    return _length;
+}
+
+template<class T>
+inline int RingBuffer<T>::read(T* _buffer, size_t _length) {
+    if (_length > readable_length) {
+        return 0;
+    }
+    for (size_t i = 0; i < _length; i++) {
+        _buffer[i] = buffer[(i + read_index) % length];
+    }
+    readable_length -= _length;
+    read_index = (read_index + _length) % length;
+    return _length;
+}
+
+template<class T>
+inline size_t RingBuffer<T>::readable() {
+    return readable_length;
+}
+
+template<class T>
+inline void RingBuffer<T>::reset() {
+    read_index = 0;
+    write_index = 0;
+    readable_length = 0;
+}
+
+template<class T>
+inline void RingBuffer<T>::flush() {
+    for (size_t i = 0; i < length; i++) {
+        buffer[i] = (T)0;
+    }
+    reset();
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,206 @@
+#include <dpp/dpp.h>
+#include <RtAudio.h>
+#include <RingBuffer.h>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+#define MAX_CHANNELS                16
+#define BUFFER_SIZE_COEFF           1024
+#define PREFERRED_BUFFER_SIZE       128
+#define PREFERRED_SAMPLE_RATE       48000
+
+typedef struct ChannelData
+{
+    dpp::snowflake user_id;
+    bool acquired;
+    const size_t channel;
+    RingBuffer<int16_t> ring_buffer;
+} ChannelData_t;
+
+inline int get_channel_num(ChannelData_t** channels, dpp::snowflake user_id) {
+    for (size_t i = 0; i < MAX_CHANNELS; i++) {
+        if (channels[i]->user_id == user_id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+inline int claim_empty_channel(ChannelData_t** channels, dpp::snowflake user_id) {
+    for (size_t i = 0; i < MAX_CHANNELS; i++) {
+        if(!channels[i]->acquired) {
+            channels[i]->user_id = user_id;
+            channels[i]->acquired = true;
+            channels[i]->ring_buffer.flush();
+            return channels[i]->channel;
+        }
+    }
+    return -1;
+}
+
+inline int claim_channel(ChannelData_t** channels, dpp::snowflake user_id, size_t channel) {
+    if (channel < MAX_CHANNELS) {
+        channels[channel]->user_id = user_id;
+        channels[channel]->acquired = true;
+        channels[channel]->ring_buffer.flush();
+        return channels[channel]->channel;
+    }
+    return -1;
+}
+
+inline void unclaim_channel_with_userid(ChannelData_t** channels, dpp::snowflake user_id) {
+    for (size_t i = 0; i < MAX_CHANNELS; i++) {
+        if (channels[i]->user_id == user_id) {
+            channels[i]->acquired = false;
+        }
+    }
+}
+
+inline void unclaim_channel_with_channelnum(ChannelData_t** channels, size_t channel) {
+    if (channel < MAX_CHANNELS) {
+        channels[channel]->acquired = false;
+    }
+}
+
+inline void unclaim_channel_all(ChannelData_t** channels) {
+    for (size_t i = 0; i < MAX_CHANNELS; i++) {
+        channels[i]->acquired = false;
+    }
+}
+
+int audio_callback(
+    void* output_buffer, 
+    void* input_buffer, 
+    unsigned int frames, 
+    double stream_time, 
+    RtAudioStreamStatus status, 
+    void* user_data) {
+        ChannelData_t** channels = (ChannelData_t**)user_data;
+        int16_t* input = (int16_t*)input_buffer;
+        int16_t* output = (int16_t*)output_buffer;
+
+        for (size_t channel = 0; channel < MAX_CHANNELS; channel++) {
+            channels[channel]->ring_buffer.read((output + (channel * frames)), frames);
+        }
+
+        return 0;
+}
+
+void error_callback(RtAudioError::Type type, const std::string &errorText) {
+    std::cout << "RtAudio Error: " << errorText << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    RtAudio audio(RtAudio::Api::WINDOWS_ASIO);
+    RtAudio::StreamParameters input_parameters;
+    RtAudio::StreamParameters output_parameters;
+    RtAudio::StreamOptions stream_options;
+    
+    unsigned int* buffer_size = new unsigned int;
+    *buffer_size = PREFERRED_BUFFER_SIZE; 
+    
+    ChannelData_t** channels = new ChannelData_t*[MAX_CHANNELS];
+    int16_t* conversion_buffer = new int16_t[65536];
+
+    for(unsigned int i = 0; i < audio.getDeviceCount(); i++) {
+        auto info = audio.getDeviceInfo(i);
+        if(info.probed == true) {
+            if (info.name.compare("ReaRoute ASIO (x64)") == 0) {
+                input_parameters.deviceId = i;
+                input_parameters.firstChannel = 0;
+                input_parameters.nChannels = info.inputChannels;
+                
+                output_parameters.deviceId = i;
+                output_parameters.firstChannel = 0;
+                output_parameters.nChannels = info.outputChannels;
+                
+                stream_options.flags = RTAUDIO_NONINTERLEAVED;
+
+                audio.openStream(
+                    &output_parameters, 
+                    &input_parameters, 
+                    RTAUDIO_SINT16, 
+                    PREFERRED_SAMPLE_RATE, 
+                    buffer_size, 
+                    audio_callback, 
+                    (void*)channels, 
+                    &stream_options
+                );
+                break;
+            }
+        }
+    }
+    
+    for (size_t i = 0; i < MAX_CHANNELS; i++) {
+        ChannelData_t* channel = new ChannelData_t{
+            0, 
+            false,
+            i,
+            RingBuffer<int16_t>(*(buffer_size) * BUFFER_SIZE_COEFF)
+        };
+        channels[i] = channel;
+    }
+    audio.startStream();
+ 
+    dpp::cluster bot("OTgwMDQ2MjQxNjM3NDcwMjg4.G6MTfy.-KcwWS9nXUr1cNGKoUP7tJibCWTpMRitn4N21Q", dpp::i_default_intents | dpp::i_message_content | dpp::i_guild_members);
+ 
+    bot.on_log(dpp::utility::cout_logger());
+ 
+    /* Use the on_message_create event to look for commands */
+    bot.on_message_create([&bot, &channels](const dpp::message_create_t & event) {
+ 
+        std::stringstream ss(event.msg.content);
+        std::string command;
+ 
+        ss >> command;
+ 
+        /* Tell the bot to record */
+        if (command == ".join") {
+            dpp::guild * g = dpp::find_guild(event.msg.guild_id);
+            if (!g->connect_member_voice(event.msg.author.id)) {
+                bot.message_create(dpp::message(
+                    event.msg.channel_id, 
+                    "You don't seem to be on a voice channel! :("
+                ));
+            }
+        }
+ 
+        /* Tell the bot to stop recording */
+        if (command == ".stop") {
+            event.from->disconnect_voice(event.msg.guild_id);
+            unclaim_channel_all(channels);
+        }
+
+        if (command == ".info") {
+            std::ostringstream os;
+            os << event.msg.author.id;
+            bot.message_create(dpp::message(
+                event.msg.channel_id,
+                os.str()
+            ));
+        }
+    });
+ 
+    bot.on_voice_receive([&bot, &channels, &conversion_buffer](const dpp::voice_receive_t &event) {
+        int channel = get_channel_num(channels, event.user_id);
+        if (channel < 0) {
+            channel = claim_empty_channel(channels, event.user_id);
+        }
+        if (channel >= 0) {
+            int16_t* data = (int16_t*)event.audio_data.data();
+            for (size_t i = 0; i < event.audio_data.length() >> 1; i = i + 2) {
+                conversion_buffer[i >> 1] = (data[i] + data[i + 1]) >> 1;
+            }
+            channels[channel]->ring_buffer.write(conversion_buffer, event.audio_data.size() >> 2);
+        }        
+    });
+ 
+    /* Start bot */
+    bot.start(false);
+    audio.stopStream();
+    audio.closeStream();
+    return 0;
+}
